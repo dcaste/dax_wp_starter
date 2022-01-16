3561c24a7c265fd85447734f16279afc3ce6e59a
dcaste <dax@daxcastellon.dev>
2022-01-16 19:57:37 +0100

    Updated gulp files

--- functions.php
+++ functions.php
@@ -11,5 +11,3 @@ require get_template_directory() . '/functions/helpers.php';
 require get_template_directory() . '/functions/menu.php';
 require get_template_directory() . '/functions/sidebars.php';
 require get_template_directory() . '/functions/thumbnails.php';
-require get_template_directory() . '/functions/pagination.php';
-require get_template_directory() . '/functions/acf.php';
